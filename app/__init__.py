2894ba0a173a4806baf62e2fb6ba3aac2079fd03
kangangi <wanjikukangangi@gmail.com>
2020-05-03 21:38:55 +0300

    create views and the navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app(config_name):
 
     #initializing flask extenxtion
     db.init_app(app)
+    bootstrap.init_app(app)
 
     #Registering blueprints
     from .main import main as main_blueprint
